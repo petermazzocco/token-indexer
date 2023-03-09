6abed086ad98254b438e8a40f56cf8cb045e33a1
PM <100664895+OhItsPM@users.noreply.github.com>
2023-03-09 07:47:26 -0800

    Style changes

--- src/App.js
+++ src/App.js
@@ -33,85 +33,90 @@ const App = () => {
   }
 
   return (
-    <div className="min-h-screen bg-black text-white bg-[url('../public/bg.svg')] bg-fixed">
-      <div className="flex flex-col align-middle justify-center px-24 py-24">
-        <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            VAULT
-          </span>
-          -20
-        </h1>
-        <h2 className="text-xl">
-          An Easy Way To View Your{" "}
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            ERC-20
-          </span>{" "}
-          Assets
-        </h2>
-      </div>
-      <div className="flex sm:flex-col md:flex-row justify-evenly">
-        <div className="justify-center">
-          <h2 className=" text-xl font-bold mt-8 mb-1">Enter A Address</h2>
-          <input
-            class="relative inline-flex w-full items-center text-black border-white justify-center p-0.5 mb-2 mr-2 hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800 overflow-hidden text-sm font-medium border-2 rounded-lg "
-            onChange={(e) => setUserAddress(e.target.value)}
-          />
-          <button
-            class="relative inline-flex h-11 w-full items-center border-white justify-center p-0.5 mb-2 mr-2 hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800 overflow-hidden text-sm font-medium border-2 rounded-lg "
-            onClick={getTokenBalance}
-          >
-            Submit
-          </button>
-        </div>
-        <div className="grid justify-center align-middle mt-4 pb-10">
-          {hasQueried ? (
-            <div class="relative overflow-x-auto flex flex-col align-middle place-items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <table className=" text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" class="px-6 py-3">
-                      Symbol
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Logo
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Balance
-                    </th>
-                  </tr>
-                </thead>
-                {results.tokenBalances.map((e, i) => {
-                  return (
-                    <tbody>
-                      <td className="px-6 py-4 border border-gray-600 font-medium whitespace-nowrap ">
-                        {tokenDataObjects[i].symbol}
-                        &nbsp;
-                      </td>
-                      <td className="px-6 py-4 border border-gray-600 font-medium  whitespace-nowrap ">
-                        {tokenDataObjects[i].logo ? (
-                          <img
-                            src={tokenDataObjects[i].logo}
-                            alt="logo"
-                            className="w-8"
-                          />
-                        ) : (
-                          <p>N/A</p>
-                        )}
-                      </td>
-                      <td className="px-6 py-4 border border-gray-600 font-medium whitespace-nowrap ">
-                        {Utils.formatUnits(
-                          e.tokenBalance,
-                          tokenDataObjects[i].decimals
-                        ).substring(0, 8)}
-                      </td>
-                    </tbody>
-                  );
-                })}
-              </table>
+    <div className="min-h-max bg-black text-white bg-[url('../public/bg.svg')] bg-fixed ">
+      <div className="h-screen grid place-items-center">
+        <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-24 pl-10 pr-10 place-content-center">
+          <div className=" md:col-span-1">
+            <div className="flex flex-col">
+              <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                  VAULT
+                </span>
+                -20
+              </h1>
+              <h2 className="text-xl">
+                An Easy Way To View Your{" "}
+                <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                  ERC-20
+                </span>{" "}
+                Assets
+              </h2>
+              <h2 className=" text-xl font-bold mt-8 mb-1">Enter A Address</h2>
+              <input
+                className="relative inline-flex w-full items-center text-black border-white justify-center p-0.5 mb-2 mr-2 hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800 overflow-hidden text-sm font-medium border-2 rounded-lg "
+                onChange={(e) => setUserAddress(e.target.value)}
+              />
+              <button
+                className="relative inline-flex h-11 w-full items-center border-white justify-center p-0.5 mb-2 mr-2 hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800 overflow-hidden text-sm font-medium border-2 rounded-lg "
+                onClick={getTokenBalance}
+              >
+                Submit
+              </button>
             </div>
-          ) : (
-            <p></p>
-          )}
+          </div>
+          <div className="md:col-span-1">
+            {hasQueried ? (
+              <div
+                className="relative overflow-x-auto overflow-y-auto
+               flex flex-col bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              >
+                <table className=" text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Symbol
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Logo
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Balance
+                      </th>
+                    </tr>
+                  </thead>
+                  {results.tokenBalances.map((e, i) => {
+                    return (
+                      <tbody>
+                        <td className="px-6 py-4 border border-gray-600 font-medium whitespace-nowrap ">
+                          {tokenDataObjects[i].symbol.substring(0, 7)}
+                          &nbsp;
+                        </td>
+                        <td className="px-6 py-4 border border-gray-600 font-medium  whitespace-nowrap ">
+                          {tokenDataObjects[i].logo ? (
+                            <img
+                              src={tokenDataObjects[i].logo}
+                              alt="logo"
+                              className="w-8"
+                            />
+                          ) : (
+                            <p>N/A</p>
+                          )}
+                        </td>
+                        <td className="px-6 py-4 border border-gray-600 font-medium whitespace-nowrap ">
+                          {Utils.formatUnits(
+                            e.tokenBalance,
+                            tokenDataObjects[i].decimals
+                          ).substring(0, 8)}
+                        </td>
+                      </tbody>
+                    );
+                  })}
+                </table>
+              </div>
+            ) : (
+              <p></p>
+            )}
+          </div>
         </div>
       </div>
     </div>
